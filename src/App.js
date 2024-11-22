da3a3bd5f641cce89170e43548f036ca83ecacd1
PremKris <premkrishna222324@gmail.com>
2024-11-22 10:58:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+// import Navbar from "./components/Navbar"; //importing components
+
+function App(props) {
+  return (
+    <>
+      
+      <Navbar title="Prem" home="kajsn" about="akjsn"/>  
+    </>
+  );
+}
+
+export default App;
